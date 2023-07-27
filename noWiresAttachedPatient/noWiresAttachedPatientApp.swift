ddac9a596a199a2885de52d4c33274cf996ac42d
MJ117919 <maryjo@c15.io>
2023-07-27 08:35:58 -0400

    Initial Commit

---
+++ noWiresAttachedPatient/noWiresAttachedPatientApp.swift
@@ -0,0 +1,17 @@
+//
+//  noWiresAttachedPatientApp.swift
+//  noWiresAttachedPatient
+//
+//  Created by MJ Ajiduah on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct noWiresAttachedPatientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
